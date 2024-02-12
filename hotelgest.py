30abc20733a5608691af767f0c0287c6c54dc8ec
Alejandro Gómez Lozano <alejandrogomezlozano@hotmail.com>
2024-02-12 20:58:50 +0100

    Divided in Classes

--- hotelgest.py
+++ hotelgest.py
@@ -56,100 +56,110 @@ class error(book):
     def descripcion(self):
         return "Error"
 
-#Abre el archivo de entrada
-file_in = "in1.xlsx"
-wb = openpyxl.load_workbook(file_in)
-hoja = wb.active
-last = hoja.max_row
+class read_excel:
+    def __init__(self, file_in):
+        self.file_in = file_in
+        self.platforms = {
+        "airbnb": airbnb,
+        "booking": booking,
+        "mara": web
+        }
 
-#Crea un diccionario con las distintas plataformas
-platforms = {
-    "airbnb": airbnb,
-    "booking": booking,
-    "mara": web
-}
+    def read_excel(self):
+        wb = openpyxl.load_workbook(self.file_in)
+        hoja = wb.active
+        last = hoja.max_row
+        
+        reservas = []
 
-#Creaun array vacio donde se guardaran las reservas independientemente de su plataforma
-reservas = []
+        for i in range(1,last):
+            fecha = str(hoja.cell(i+1,5).value)
+            factura = "MBA24/" + str(hoja.cell(i+1,4).value)
+            nombre = hoja.cell(i+1,14).value
+            NIF = hoja.cell(i+1,15).value
+            Base_1 = hoja.cell(i+1,18).value
+            Cuota_1 = hoja.cell(i+1,19).value
+            total = hoja.cell(i+1,20).value
+            domicilio = hoja.cell(i+1,11).value
+            cod_postal = hoja.cell(i+1,12).value
+            pais = hoja.cell(i+1,10).value
+            CL = 2
+            observaciones = hoja.cell(i+1,26).value
+            tipo_Sii = 1
 
-for i in range(1,last):
-    fecha = str(hoja.cell(i+1,5).value)
-    factura = "MBA24/" + str(hoja.cell(i+1,4).value)
-    nombre = hoja.cell(i+1,14).value
-    NIF = hoja.cell(i+1,15).value
-    Base_1 = hoja.cell(i+1,18).value
-    Cuota_1 = hoja.cell(i+1,19).value
-    total = hoja.cell(i+1,20).value
-    domicilio = hoja.cell(i+1,11).value
-    cod_postal = hoja.cell(i+1,12).value
-    pais = hoja.cell(i+1,10).value
-    CL = 2
-    observaciones = hoja.cell(i+1,26).value
-    tipo_Sii = 1
+            #Intenta crear las reservas. Si hay carácteres fantasmas continua a la siguiente iteración
+            try:
+                obs=observaciones.lower()
+                #Comprueba en la lista de plataformas si coincide con las observaciones
+                #Esto se puede optimizar??
+                for i in self.platforms:
+                    if i in obs:
+                        #Hace "plataformas[i]" porque i es un string del diccionario e platforms[i] es el objeto
+                        reservas.append(self.platforms[i](fecha, factura, nombre, NIF, Base_1, Cuota_1, total, domicilio, cod_postal, pais, CL, observaciones, 430000, tipo_Sii))
+            except AttributeError:
+                continue
 
-    #Intenta crear las reservas. Si hay carácteres fantasmas continua a la siguiente iteración
-    try:
-        obs=observaciones.lower()
-        #Comprueba en la lista de plataformas si coincide con las observaciones
-        #Esto se puede optimizar??
-        for i in platforms:
-            if i in obs:
-                #Hace "plataformas[i]" porque i es un string del diccionario e platforms[i] es el objeto
-                reservas.append(platforms[i](fecha, factura, nombre, NIF, Base_1, Cuota_1, total, domicilio, cod_postal, pais, CL, observaciones, 430000, tipo_Sii))
-    except AttributeError:
-        continue
+        wb.close()
+        return reservas
 
-wb.close()
+class write_excel:
 
+    def __init__(self, reservas):
+        self.reservas = reservas
 
-#Convierte los archivos de .xlsx a .xls
-def conv(file):
-    df = pd.read_excel(file, engine='openpyxl')
-    file_new = file[:-5] + ".xls"
-    with pd.ExcelWriter(file_new, engine='openpyxl') as writer:
-        df.to_excel(writer, index=False)
-    os.remove(file)
-
-def write (book):
+    def empty_out(self):
+        try:
+            shutil.rmtree("out")
+        except:
+            pass
+        os.mkdir("out")
     
-    clase = str(type(book).__name__)                ##Dudeisicon
-    file_out = "out/" + clase + ".xlsx"
+    def conv(self, file):
+        df = pd.read_excel(file, engine='openpyxl')
+        file_new = file[:-5] + ".xls"
+        with pd.ExcelWriter(file_new, engine='openpyxl') as writer:
+            df.to_excel(writer, index=False)
+        os.remove(file)
+        print(f"Archivo {file_new} convertido a .xls")
 
-    try:
-        wb = openpyxl.load_workbook(file_out)
-    except FileNotFoundError:
-        wb = Workbook()
+    def write (self):
+        self.empty_out()
+        for i in self.reservas:
+            clase = str(type(i).__name__)               
+            file_out = "out/" + clase + ".xlsx"
 
-    hoja = wb.active
-    last = hoja.max_row
+            try:
+                wb = openpyxl.load_workbook(file_out)
+            except FileNotFoundError:
+                wb = Workbook()
 
-    hoja.cell(last+1,1).value = i.fecha
-    hoja.cell(last+1,2).value = i.factura
-    hoja.cell(last+1,3).value = i.nombre
-    hoja.cell(last+1,4).value = i.NIF
-    hoja.cell(last+1,5).value = i.Base_1
-    hoja.cell(last+1,7).value = i.Cuota_1
-    hoja.cell(last+1,8).value = i.total
-    hoja.cell(last+1,9).value = i.domicilio
-    hoja.cell(last+1,10).value = i.cod_postal
-    hoja.cell(last+1,11).value = i.pais
-    hoja.cell(last+1,12).value = i.CL
-    hoja.cell(last+1,14).value = i.observaciones
-    hoja.cell(last+1,15).value = i.cuenta_contable
-    hoja.cell(last+1,16).value = i.tipo_Sii
-    last += 1
-    print(f"Factura {i.factura} añadida")
+            hoja = wb.active
+            last = hoja.max_row
 
-    wb.save(file_out)
-    wb.close()
-    print(f"Archivo {file_out} guardado con {last} facturas\n\n")
+            hoja.cell(last+1,1).value = i.fecha
+            hoja.cell(last+1,2).value = i.factura
+            hoja.cell(last+1,3).value = i.nombre
+            hoja.cell(last+1,4).value = i.NIF
+            hoja.cell(last+1,5).value = i.Base_1
+            hoja.cell(last+1,7).value = i.Cuota_1
+            hoja.cell(last+1,8).value = i.total
+            hoja.cell(last+1,9).value = i.domicilio
+            hoja.cell(last+1,10).value = i.cod_postal
+            hoja.cell(last+1,11).value = i.pais
+            hoja.cell(last+1,12).value = i.CL
+            hoja.cell(last+1,14).value = i.observaciones
+            hoja.cell(last+1,15).value = i.cuenta_contable
+            hoja.cell(last+1,16).value = i.tipo_Sii
+            last += 1
+            wb.save(file_out)
+            wb.close()
+            print(f"Factura {i.factura} añadida a archivo {file_out}")
+        
+        for element in os.listdir("out"):
+            self.conv("out/"+element)
 
 
-shutil.rmtree("out")
-os.mkdir("out")
 
 
-for i in reservas:
-    write(i)
-for element in os.listdir("out"):
-    conv("out/"+element)
+excel = read_excel("in1.xlsx").read_excel()
+write = write_excel(excel).write()
